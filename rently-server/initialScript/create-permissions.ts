6ad5ee866dafe8224a467cdb91334c4cc7ae6807
Tran Anh Hao <Trananhhao1307@gmail.com>
2025-03-15 11:58:40 +0700

    Update: Tạo script xóa hoặc tạo permission dựa trên các endpoint hiện có

--- rently-server/initialScript/create-permissions.ts
+++ rently-server/initialScript/create-permissions.ts
@@ -8,10 +8,21 @@ const prisma = new PrismaService();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3010);
+
   const server = app.getHttpAdapter().getInstance();
   const router = server.router;
 
-  const availableRoutes = router.stack
+  const permissionsInDb = await prisma.permission.findMany({
+    where: {
+      deletedAt: null,
+    },
+  });
+
+  const availableRoutes: {
+    path: string;
+    method: keyof typeof HTTPMethod;
+    name: string;
+  }[] = router.stack
     .map((layer) => {
       if (layer.route) {
         const path = layer.route?.path;
@@ -27,11 +38,57 @@ async function bootstrap() {
     })
     .filter((item) => item !== undefined);
 
-  const result = await prisma.permission.createMany({
-    data: availableRoutes,
-    skipDuplicates: true,
+  // Tạo object permissionInDbMap với key là [method-path]
+  const permissionInDbMap: Record<string, (typeof permissionsInDb)[0]> =
+    permissionsInDb.reduce((acc, item) => {
+      acc[`${item.method}-${item.path}`] = item;
+      return acc;
+    }, {});
+
+  // Tạo object availableRoutesMap với key là [method-path]
+  const availableRoutesMap: Record<string, (typeof availableRoutes)[0]> =
+    availableRoutes.reduce((acc, item) => {
+      acc[`${item.method}-${item.path}`] = item;
+      return acc;
+    }, {});
+
+  // Tìm permissions trong database mà không tồn tại trong availableRoutes
+  const permissionsToDelete = permissionsInDb.filter((item) => {
+    return !availableRoutesMap[`${item.method}-${item.path}`];
   });
-  console.log(result);
+
+  // Xóa permissions không tồn tại trong availableRoutes
+  if (permissionsToDelete.length > 0) {
+    const deleteResult = await prisma.permission.deleteMany({
+      where: {
+        id: {
+          in: permissionsToDelete.map((item) => item.id),
+        },
+      },
+    });
+
+    console.log('Deleted permissions:', deleteResult.count);
+  } else {
+    console.log('No permissions to delete');
+  }
+
+  // Tìm routes mà không tồn tại trong permissionsInDb
+  const routesToAdd = availableRoutes.filter((item) => {
+    return !permissionInDbMap[`${item.method}-${item.path}`];
+  });
+
+  // Thêm các routes này dưới dạng permissions database
+  if (routesToAdd.length > 0) {
+    const permissionsToAdd = await prisma.permission.createMany({
+      data: routesToAdd,
+      skipDuplicates: true,
+    });
+
+    console.log('Added permissions:', permissionsToAdd.count);
+  } else {
+    console.log('No permissions to add');
+  }
+
   process.exit(0);
 }
 bootstrap();
